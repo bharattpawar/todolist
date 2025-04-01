b215f8c1e3d86c36a67eff453874317f5a11933e
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2025-04-01 08:30:56 +0530

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Homepage from "./components/Homepage";
 import TodoTask from "./components/Todo_task";
- 
+import Login from "./components/Login";
+import Signup from "./components/signup";
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/todo" element={<TodoTask />} />
       </Routes>
     </Router>
